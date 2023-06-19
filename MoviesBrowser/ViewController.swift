68d17d405610606b8d04b7ee2c069eaee371f525
Pawel Zgoda-Ferchmin <pawel.ferchmin@hyd.agency>
2023-06-19 15:53:26 +0200

    Initial Commit

---
+++ MoviesBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesBrowser
+//
+//  Created by Paweł Zgoda-Ferchmin on 19/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
